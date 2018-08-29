20e47aecd2dfa52a1f1ec35c3729afef6f8b75d1
JunYoung-Cho <cjy8745@ilts.co.kr>
2018-08-29 18:00:49 +0900

    site select add

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { EventEmitter } from 'protractor';
 
+export interface Site {
+  name: string,
+  url: string
+}
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  site: Site[] = [
+    {
+      name: 'material',
+      url:'https://material.angular.io/components'
+    },
+    {
+      name: 'fxLayout',
+      url:'https://tburleson-layouts-demos.firebaseapp.com/#/docs'
+    },
+    {
+      name: 'angular6',
+      url:'https://angular.io'
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
+  }
+  selectChanged() {
+    alert("22")
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   exports: [
-    MatToolbarModule
+    MatToolbarModule,
+    MatSelectModule,
+    MatFormFieldModule
   ],
   declarations: []
 })

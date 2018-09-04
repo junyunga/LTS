2e0131fa8c97c7a7ad05e85b79ce4210b3d5dc48
JunYoung-Cho <cjy8745@ilts.co.kr>
2018-09-04 18:55:39 +0900

    time-wrap add

--- src/app/main-time/main-time.component.ts
+++ src/app/main-time/main-time.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-main-time',
-  templateUrl: './main-time.component.html',
-  styleUrls: ['./main-time.component.css']
+    selector: 'app-main-time',
+    templateUrl: './main-time.component.html',
+    styleUrls: ['./main-time.component.scss']
 })
 export class MainTimeComponent implements OnInit {
+    today: number = 0;
+    setTime: any = null;
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.getTodayTime();
+        this.setTime = setInterval(() => {
+            this.getTodayTime();
+        }, 1000);
+    }
+    getTodayTime(): void {
+        this.today = Date.now();
+    }
+    ngOnDestroy(): void {
+        clearInterval(this.setTime);
+    }
 
 }

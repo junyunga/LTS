af80866eeda20e1e27e3412db268d65ab2e69ab7
JunYoung-Cho <cjy8745@ilts.co.kr>
2018-09-12 18:34:51 +0900

    three cube add

--- src/app/menu/three-d-design/screens/screen-test/screen-test.component.ts
+++ src/app/menu/three-d-design/screens/screen-test/screen-test.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import * as THREE from "three";
 @Component({
-  selector: 'app-screen-test',
-  templateUrl: './screen-test.component.html',
-  styleUrls: ['./screen-test.component.scss']
+  selector: "app-screen-test",
+  templateUrl: "./screen-test.component.html",
+  styleUrls: ["./screen-test.component.scss"]
 })
 export class ScreenTestComponent implements OnInit {
-
-  constructor() { }
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  renderer = new THREE.WebGLRenderer();
+  constructor() {}
 
   ngOnInit() {
+    this.animationStart();
   }
 
+  animationStart(): void {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+
+    this.scene.add(cube);
+
+    this.camera.position.z = 5;
+
+    const test = (): void => {
+      requestAnimationFrame(test);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      this.renderer.render(this.scene, this.camera);
+    };
+    test();
+  }
 }

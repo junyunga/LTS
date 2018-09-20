66ca0fa8a61ce457a1ad18dc5f797dda17f3c83f
JunYoung-Cho <cjy8745@ilts.co.kr>
2018-09-20 18:50:45 +0900

    ex1..

--- src/app/menu/three-d-design/screens/screen-test/screen-test.component.ts
+++ src/app/menu/three-d-design/screens/screen-test/screen-test.component.ts
@@ -1,53 +1,49 @@
 import { Component, OnInit, ChangeDetectorRef } from "@angular/core";
 import * as THREE from "three";
-import { Stats } from "fs";
-import test from "three-orbit-controls";
 @Component({
-  selector: "app-screen-test",
-  templateUrl: "./screen-test.component.html",
-  styleUrls: ["./screen-test.component.scss"]
+    selector: "app-screen-test",
+    templateUrl: "./screen-test.component.html",
+    styleUrls: ["./screen-test.component.scss"]
 })
 export class ScreenTestComponent implements OnInit {
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-  renderer = new THREE.WebGLRenderer();
-  constructor() { }
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    );
+    renderer = new THREE.WebGLRenderer();
+    constructor() { }
 
-  ngOnInit() {
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(this.renderer.domElement);
-    this.camera.position.set(0, 200, 350);
-    const sun = new THREE.DirectionalLight(0xffffff, 1.0);
-    sun.position.set(300, 400, 175);
-    this.scene.add(sun);
+    ngOnInit() {
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(this.renderer.domElement);
+        this.camera.position.z = 10;
 
-    const sun2 = new THREE.DirectionalLight(0x40a040, 0.6);
-    sun2.position.set(-100,350,-200);
-    this.scene.add(sun2);
+        const material1 = new THREE.LineBasicMaterial({ color: 0xe0e0e0 });
+        const geometry1 = new THREE.Geometry();
 
-    
-    
+        for (let i = 0; i < 1500; i++) {
+          
+            let xPos = Math.random() * 20 - 10;
+            let yPos = Math.random() * 20 - 10;
+            let zPos = Math.random() * 20 - 10;
+            
+            geometry1.vertices.push(new THREE.Vector3(xPos, yPos, zPos));
+        }
+        const line = new THREE.Line(geometry1, material1);
+        this.scene.add(line);
+        this.animationStart();
+    }
 
-    this.animationStart();
-  }
-
-  animationStart(): void {
-
-
-
-
-
-
-
-
-    const test = (): void => {
-
-    };
-    test();
-  }
+    animationStart(): void {
+      
+        const animate = (): void => {
+          requestAnimationFrame( animate );
+          this.renderer.render(this.scene, this.camera);
+        };
+        animate();
+    }
+    //http://binteractive.org/?p=230
 }
